4919fa854ea12799989a547be85097479cb14475
DDZIdanek <domi.mlcoch@gmail.com>
2024-05-08 12:34:52 +0200

    Java project

---
+++ src/main/java/cz/vsb/fei/java/mlc0044_java_psp/Mlc0044JavaPspApplication.java
@@ -0,0 +1,13 @@
+package cz.vsb.fei.java.mlc0044_java_psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mlc0044JavaPspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mlc0044JavaPspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/vsb/fei/java/mlc0044_java_psp/Mlc0044JavaPspApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vsb.fei.java.mlc0044_java_psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mlc0044JavaPspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
